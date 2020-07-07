c1e7f94b8de9faf19e31898b4f3efe4f1ae945cd
CU Truck Simulator <cu.truck.sim@gmail.com>
2020-07-08 03:33:35 +0800

    Updates

---
+++ precache-manifest.aedcd4c8ac5c83d240f382b88edc5026.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59b10594d50fd8d9f9bcacd838cb2c9c",
+    "url": "/cu-truck-simulator/index.html"
+  },
+  {
+    "revision": "3c743e5741ee1772436a",
+    "url": "/cu-truck-simulator/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "71f66e889b7a8e158aee",
+    "url": "/cu-truck-simulator/static/css/main.acadef03.chunk.css"
+  },
+  {
+    "revision": "3c743e5741ee1772436a",
+    "url": "/cu-truck-simulator/static/js/2.80eaa2fc.chunk.js"
+  },
+  {
+    "revision": "ca429a12ee98e43556d1195e6b1d1e1e",
+    "url": "/cu-truck-simulator/static/js/2.80eaa2fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71f66e889b7a8e158aee",
+    "url": "/cu-truck-simulator/static/js/main.c392bff5.chunk.js"
+  },
+  {
+    "revision": "f35c783db809f5a0848e",
+    "url": "/cu-truck-simulator/static/js/runtime-main.bb324c8c.js"
+  }
+]);
